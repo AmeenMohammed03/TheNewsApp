bf8ab3b1fa515ebe38ad470c3104eca28b07e95e
Mohammed Ameen <127188975+AmeenMohammed03@users.noreply.github.com>
2024-02-26 10:17:06 +0530

    Modified API calls

--- app/src/main/java/com/example/thenewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/example/thenewsapp/api/NewsAPI.kt
@@ -10,7 +10,7 @@ interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getHeadlines(
         @Query("country")
-        countryCode: String = "us",
+        countryCode: String,
         @Query("page")
         pageNumber: Int = 1,
         @Query("apikey")
