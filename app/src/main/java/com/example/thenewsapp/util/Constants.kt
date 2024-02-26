c6c60e0de7517b6a3494f98ffe74718c629d6bb4
Mohammed Ameen <127188975+AmeenMohammed03@users.noreply.github.com>
2024-02-26 18:03:21 +0530

    Changed the API key

--- app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.thenewsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "6e56af79cd174ade935bd97c1f66ef2e"
+        const val API_KEY = "90df81da44ec450a90be6213d127649d"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
