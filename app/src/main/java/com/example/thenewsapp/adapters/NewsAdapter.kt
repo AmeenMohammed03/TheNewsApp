c26a272ff1148a9a983c5f3fe55d0c2397637231
Mohammed Ameen <127188975+AmeenMohammed03@users.noreply.github.com>
2024-03-01 18:13:01 +0530

    Refactored NewsAdapter class

--- app/src/main/java/com/example/thenewsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/thenewsapp/adapters/NewsAdapter.kt
@@ -1,20 +1,30 @@
 package com.example.thenewsapp.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.thenewsapp.R
-import com.example.thenewsapp.databinding.ItemNewsBinding
 import com.example.thenewsapp.models.Article
 
-//is responsible for adapting a list of articles into views that can be displayed within a RecyclerView
-class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
-    inner class ArticleViewHolder(val binding: ItemNewsBinding) :
-        RecyclerView.ViewHolder(binding.root)
+class NewsAdapter(private val onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    private val articles: MutableList<Article> = mutableListOf()
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val articleImage = itemView.findViewById<ImageView>(R.id.articleImage)
+        val articleSource = itemView.findViewById<TextView>(R.id.articleSource)
+        val articleTitle = itemView.findViewById<TextView>(R.id.articleTitle)
+        val articleDescription = itemView.findViewById<TextView>(R.id.articleDescription)
+        val articleDateTime = itemView.findViewById<TextView>(R.id.articleDateTime)
+        val textViewNewsLink = itemView.findViewById<TextView>(R.id.textViewNewsLink)
+    }
 
     private val differCallback = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
@@ -28,11 +38,9 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     val differ = AsyncListDiffer(this, differCallback)
 
-    private var onItemClickListener: ((Article) -> Unit)? = null
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ArticleViewHolder(binding)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        return ArticleViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -41,35 +49,25 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
-
-        with(holder.binding) {
+        holder.itemView.apply {
             if (article.urlToImage.isNullOrEmpty()) {
-                Glide.with(root)
-                    .load(R.drawable.placeholder_image)
-                    .into(articleImage)
+                Glide.with(this).load(R.drawable.placeholder_image).into(holder.articleImage)
             } else {
-                Glide.with(root)
-                    .load(article.urlToImage)
-                    .into(articleImage)
+                Glide.with(this).load(article.urlToImage).into(holder.articleImage)
             }
+            holder.articleSource.text = article.source?.name
+            holder.articleTitle.text = article.title
+            holder.articleDescription.text = article.description
+            holder.articleDateTime.text = article.publishedAt
+            holder.textViewNewsLink.text = "Read more..."
 
-            articleSource.text = article.source?.name
-            articleTitle.text = article.title
-            articleDescription.text = article.description
-            articleDateTime.text = article.publishedAt
-            textViewNewsLink.text = "Read more..."
-
-            root.setOnClickListener {
-                onItemClickListener?.invoke(article)
-            }
-            textViewNewsLink.setOnClickListener {
-                onItemClickListener?.invoke(article)
+            holder.textViewNewsLink.setOnClickListener {
+                onItemClickListener.onItemClick(article)
             }
         }
     }
 
-
-    fun setOnItemClickListener(listener: (Article) -> Unit) {
-        onItemClickListener = listener
+    interface OnItemClickListener {
+        fun onItemClick(article: Article)
     }
 }
